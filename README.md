b998fcd7afa8b96429669178189c64bef25612bd
Rajan9176 <114674358+Rajan9176@users.noreply.github.com>
2022-09-29 07:37:20 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-wqgaht
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-wqgaht)
---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hellotttt StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+    </div>
+  );
+}
