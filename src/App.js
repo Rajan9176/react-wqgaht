f434d0f2384bd5f65f4601c98aa337f03abe99c4
Rajan9176 <114674358+Rajan9176@users.noreply.github.com>
2022-09-30 07:04:33 -0700

    loginpage.jsx

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './style.css';
 import Signup from './pages/signup';
+import Login from './pages/login';
 
 export default function App() {
   return (
     <div>
       <Signup></Signup>
+      <Login></Login>
       <h1>Hellotttt StackBleeitz!</h1>
       <p>Start gfggfediting to see rajan some magic happen :)</p>
     </div>
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,27 +1,17 @@
+import React from 'react';
 function Login() {
   return (
     <div>
-      <div className="Sign-Up-Form">
-        <h1>Sign Up Now</h1>
-        <form>
-          <input type="User Name" class="input-box" placeholder="User Name" />
-          <br />
-          <br />
-          <input type="Email" class="input-box" placeholder="Your Email" />
-          <br />
-          <br />
-          <input type="Password" class="input-box" placeholder="Password" />
-          <br />
-          <br />
-          <input
-            type="Password"
-            class="Re-Password"
-            placeholder="Re-Password"
-          />
-          <br />
-          <br />
-          <button>Sign</button>
-        </form>
+      <div>
+        <h1>Login Page</h1>
+        <label htmlfor="username">username</label>
+        <input type="text" />
+        <br />
+        <br />
+        <label htmlfor="password">password</label>
+        <input type="password" />
+        <br />
+        <br /> <button>login</button>
       </div>
     </div>
   );
