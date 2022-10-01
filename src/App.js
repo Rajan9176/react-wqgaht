ac3ee9bd285ff1e6c7d4ed5eac713f0e480d927b
Rajan9176 <114674358+Rajan9176@users.noreply.github.com>
2022-09-30 22:37:14 -0700

    counter.jsx

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import React from 'react';
 import './style.css';
 import Signup from './pages/signup';
 import Login from './pages/login';
-
+import Counter from './pages/counter';
 export default function App() {
   return (
     <div>
       <Signup></Signup>
       <Login></Login>
+      <Counter></Counter>
       <h1>Hellotttt StackBleeitz!</h1>
       <p>Start gfggfediting to see rajan some magic happen :)</p>
     </div>
--- src/pages/counter.jsx
+++ src/pages/counter.jsx
@@ -1,4 +1,16 @@
-<div>
-  <h1>COUNTER</h1>
-  <button>-</button>0<button>+</button>
-</div>;
+import React from 'react';
+function Counter() {
+  const [count, setCount] = React.useState(1);
+  const increment = () => {
+    setCount(count + 1);
+  };
+  return (
+    <div>
+      <div>
+        <h1>Counter</h1>
+        <button>-</button>0<button onClick={() => increment()}>+</button>
+      </div>
+    </div>
+  );
+}
+export default Counter;
