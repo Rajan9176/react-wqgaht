c111a8d1a287525ab99dddf5e4e32f1c899183da
Rajan9176 <114674358+Rajan9176@users.noreply.github.com>
2022-10-04 02:31:47 -0700

    chat

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import './style.css';
 import Signup from './pages/signup';
 import Login from './pages/login';
 import Counter from './pages/counter';
+import Chat from './pages/chat';
 export default function App() {
   return (
     <div>
-      <Counter></Counter>
-      <Signup />
+      <Chat />
+      {/* <Signup />
+       <Login/> 
+        <Counter />  */}
+
       <h1>Hellotttt StackBleeitz!</h1>
       <p>Start gfggfediting to see rajan some magic happen :)</p>
     </div>
---
+++ src/pages/chat.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+function Chat()
+ {
+   
+return (
+  <div>
+    <div>
+      <h1>Chat Messages</h1>
+      <button>1</button>
+      Rajan
+      <button>2</button>
+    </div>
+  </div>
+);
+ }
+export default Chat;
