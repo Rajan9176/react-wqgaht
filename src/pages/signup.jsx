042f2eb9af1bc3a41d446f7279376b940a62e406
Rajan9176 <114674358+Rajan9176@users.noreply.github.com>
2022-10-10 15:22:35 +0530

    signup.css

--- src/pages/signup.jsx
+++ src/pages/signup.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 function Signup() {
   return (
+    
     <div className="mani">
-      
         <h1 className="raj">Sign Up Now</h1>
-        <form>
+       
+        <form className="rajan">
           <label className="rajan">
-          <input type="User Name"className="input-box"placeholder="User Name"/><br/><br/>
-          <input type="Email" className="input-box" placeholder="YouEmail" /> <br/><br/>
-          <input type="Password" className="input-box" placeholder="Password" /><br/> <br/>
-           <input type="Password"className="Re-Password"placeholder="Re-Password"/></label>
+          <input type="User Name"placeholder="User Name"/><br/><br/>
+          <input type="Email"  placeholder="YouEmail" /> <br/><br/>
+          <input type="Password" placeholder="Password" /><br/> <br/>
+           <input type="Password"placeholder="Re-Password"/></label>
           <br />
           <br />
           <button className="ra">Sign</button>
