228ae3b91066ad367d28d32f7db93f5a1d2e673c
Rajan9176 <114674358+Rajan9176@users.noreply.github.com>
2022-10-12 19:26:01 +0530

    navbar.jsx

--- src/pages/navbar.jsx
+++ src/pages/navbar.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import '../css/navbar.css'
+import {Link} from 'react-router-dom';
 function Navbar() {
   return(
 <nav>
   <ul>
-    <li> <a href="/login">Login</a></li>
-    <li> <a href="/signup">Signup</a></li>
-    <li> <a href="/counter">Counter</a></li>
-    <li> <a href="/employeecard">EmployeeCard</a></li>
-    <li> <a href="/chat">Chat</a></li>
-    <li> <a href="/calculator">Calculator</a></li>
+    <li> <Link to="/login">Login</Link></li>
+    <li> <Link to="/signup">Signup</Link></li>
+    <li> <Link to="/counter">Counter</Link></li>
+    <li> <Link to="/employeecard">EmployeeCard</Link></li>
+    <li> <Link to="/chat">Chat</Link></li>
+    <li> <Link to="/calculator">Calculator</Link></li>
 
 
   </ul>
