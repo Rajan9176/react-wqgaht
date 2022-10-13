3f5b68816f32e74194fafb187a51c994334019b5
Rajan9176 <114674358+Rajan9176@users.noreply.github.com>
2022-10-13 09:10:30 +0530

    navbar.jsx

--- src/pages/navbar.jsx
+++ src/pages/navbar.jsx
@@ -5,12 +5,12 @@ function Navbar() {
   return(
 <nav>
   <ul>
-    <li> <Link to="/login">Login</Link></li>
-    <li> <Link to="/signup">Signup</Link></li>
-    <li> <Link to="/counter">Counter</Link></li>
-    <li> <Link to="/employeecard">EmployeeCard</Link></li>
-    <li> <Link to="/chat">Chat</Link></li>
-    <li> <Link to="/calculator">Calculator</Link></li>
+    <li> <Link to="/login"><i class="fa-solid fa-user"></i> Login</Link></li>
+    <li> <Link to="/signup"><i class="fa-solid fa-user-plus"></i> Signup</Link></li>
+    <li> <Link to="/counter"><i class="fa-solid fa-earth-africa"></i> Counter</Link></li>
+    <li> <Link to="/employeecard"><i class="fa-solid fa-id-card"></i>EmployeeCard</Link></li>
+    <li> <Link to="/chat"><i class="fa-solid fa-comment"></i> Chat</Link></li>
+    <li> <Link to="/calculator"><i class="fa-solid fa-calculator"></i>Calculator</Link></li>
 
 
   </ul>
