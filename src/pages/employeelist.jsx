2607c763a8305b5afc0bf7c28a0578398b29205e
Rajan9176 <114674358+Rajan9176@users.noreply.github.com>
2022-10-14 20:24:45 +0530

    employeelist.jsx

--- src/pages/employeelist.jsx
+++ src/pages/employeelist.jsx
@@ -2,11 +2,15 @@ import React, { Component } from 'react';
 class EmployeeList extends Component {
   buttons = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   genarateButtons() {
-    let displayButtons = '';
-    for (let i = 1; i <= this.buttons.length; i++) {
-      displayButtons += '<button>' + i + '</button>';
-    }
-    return displayButtons;
+    return this.buttons.map((each) => {
+      // return '<button>' + each + '</button>';
+      return <button>{each}</button>
+    });
+    //   let displayButtons = '';
+    //   for (let i = 1; i <= this.buttons.length; i++) {
+    //     displayButtons += '<button>' + i + '</button>';
+    //   }
+    //   return displayButtons;
   }
   render() {
     return (
