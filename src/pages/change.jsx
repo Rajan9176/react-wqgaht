cd8521e3730f07a7bfec7bbf6cb6b1daf442b73c
Rajan9176 <114674358+Rajan9176@users.noreply.github.com>
2022-10-16 10:48:29 +0530

    change.jsx

--- src/pages/change.jsx
+++ src/pages/change.jsx
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import '../css/change.css';
 class Change extends Component {
- 
+  state={
+    count:25
+  }
+  increment(){
+  //  alert(this.state.count);
+   this.setState({count:this.state.count+20});
+  }
   render() {
     return (
       <div className="change">
-        <button>Change</button>
+        <button onClick={()=>this.increment()}>Change</button>
         <tr>
           <th className="name">Name</th>
           <th className="name">Age</th>
