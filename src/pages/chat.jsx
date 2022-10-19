37499a5ed8c5a7520aa322e5703b8c20548d8e22
Rajan9176 <114674358+Rajan9176@users.noreply.github.com>
2022-10-19 15:26:14 +0530

    chat.jsx

--- src/pages/chat.jsx
+++ src/pages/chat.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
+import '../css/chat.css';
 import { useState } from 'react';
 function Chat() {
   const [message, setMessage] = useState('0');
   return (
-    <div>
-      <div>
-        <h1>Chat Messages</h1>
-        <button onClick={() => setMessage('hi welcome')}>1</button>
-        <lable>{message}</lable>
-        <button onClick={() => setMessage('i am good boy')}>2</button>
-      </div>
+    <div className="message">
+      <h1 className="hh">Chat Messages</h1>
+      <button onClick={() => setMessage('hi welcome')}>1</button>
+      <lable>{message}</lable>
+      <button onClick={() => setMessage('i am good boy')}>2</button>
     </div>
   );
 }
