c057c6188fabdda53520fbcfa94689e2ac81a767
Rajan9176 <114674358+Rajan9176@users.noreply.github.com>
2022-10-19 10:41:06 +0530

    TitleCounter.jsx

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import './style.css';
+import React from './style.css';
+import Navbar from './pages/navbar';
 import Signup from './pages/signup';
 import Login from './pages/login';
 import Counter from './pages/counter';
 import Chat from './pages/chat';
-import EmployeeCard from './pages/employee';
-import Calcultor from './pages/calculator';
-import Navbar from './pages/navbar';
+import EmployeeCard from './pages/employeecard';
+import Calculator from './pages/calculator';
 import Dummy from './pages/dummy';
 import Timer from './pages/timer';
 import ErrorPage from './pages/errorpage';
@@ -14,7 +14,7 @@ import EmployeeDetails from './pages/employeedetails';
 import SimpleClass from './pages/simpleClass';
 import EmployeeList from './pages/employeelist';
 import Change from './pages/change';
-import UseEffect from './pages/useeffect';
+import TitleCounter from './pages/titlecounter';
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 export default function App() {
@@ -25,17 +25,17 @@ export default function App() {
         <Route path="login" element={<Login />} />
         <Route path="signup" element={<Signup />} />
         <Route path="counter" element={<Counter />} />
-        <Route path="EmployeeCard" element={<EmployeeCard />} />
         <Route path="chat" element={<Chat />} />
-        <Route path="calculator" element={<Calcultor />} />
+        <Route path="employeeCard" element={<EmployeeCard />} />
+        <Route path="calculator" element={<Calculator />} />
         <Route path="dummy" element={<Dummy />} />
+        <Route path="timer" element={<Timer />} />
         <Route path="errorpage" element={<ErrorPage />} />
         <Route path="employeedetails" element={<EmployeeDetails />} />
-        <Route path="simpleclass" element={<SimpleClass />} />
-        <Route path="timer" element={<Timer />} />
+        <Route path="simpleClass" element={<SimpleClass />} />
         <Route path="employeelist" element={<EmployeeList />} />
         <Route path="change" element={<Change />} />
-        <Route path="useeffect" element={<UseEffect />} />
+        <Route path="titlecounter" element={<TitleCounter />} />
       </Routes>
       {/* <Login />
       <Signup /> */}
--- src/pages/navbar.jsx
+++ src/pages/navbar.jsx
@@ -1,32 +1,91 @@
 import React from 'react';
 import '../css/navbar.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 function Navbar() {
-  return(
-<nav>
-  <ul>
-    <li> <Link to="/login"><i className="fa-solid fa-user"></i> Login</Link></li>
-    <li> <Link to="/signup"><i className="fa-solid fa-user-plus"></i> Signup</Link></li>
-    <li> <Link to="/counter"><i className="fa-solid fa-earth-africa"></i> Counter</Link></li>
-    <li> <Link to="/employeecard"><i className="fa-solid fa-id-card"></i> EmployeeCard</Link></li>
-    <li> <Link to="/chat"><i className="fa-solid fa-comment"></i> Chat</Link></li>
-    <li> <Link to="/calculator"><i className="fa-solid fa-calculator"></i> Calculator</Link></li>
-    <li> <Link to="/errorpage"><i className="fa-solid fa-circle-exclamation"></i> ErrorPage</Link></li>
-    <li> <Link to="/timer"><i className="fa-sharp fa-solid fa-clock"></i> Timer</Link></li>
-    <li> <Link to="/employeedetails"><i className="fa-sharp fa-solid fa-address-card"></i> EmployeeDetails</Link></li>
-    <li> <Link to="/simpleclass"><i class="fa-solid fa-landmark"></i> SimpleClass</Link></li>
-    <li> <Link to="/dummy"><i class="fa-solid fa-circle-xmark"></i> Dummy</Link></li>
-    <li> <Link to="/employeelist"> EmployeeList</Link></li>
-    <li> <Link to="/change"> Change</Link></li>
-    <li> <Link to="/useeffect"> UseEffect</Link></li>
-
-
-
-
-
-
-  </ul>
-</nav>
+  return (
+    <nav>
+      <ul>
+        <li>
+          {' '}
+          <Link to="/login">
+            <i className="fa-solid fa-user"></i> Login
+          </Link>
+        </li>
+        <li>
+          {' '}
+          <Link to="/signup">
+            <i className="fa-solid fa-user-plus"></i> Signup
+          </Link>
+        </li>
+        <li>
+          {' '}
+          <Link to="/counter">
+            <i className="fa-solid fa-earth-africa"></i> Counter
+          </Link>
+        </li>
+        <li>
+          {' '}
+          <Link to="/employeecard">
+            <i className="fa-solid fa-id-card"></i> EmployeeCard
+          </Link>
+        </li>
+        <li>
+          {' '}
+          <Link to="/chat">
+            <i className="fa-solid fa-comment"></i> Chat
+          </Link>
+        </li>
+        <li>
+          {' '}
+          <Link to="/calculator">
+            <i className="fa-solid fa-calculator"></i> Calculator
+          </Link>
+        </li>
+        <li>
+          {' '}
+          <Link to="/errorpage">
+            <i className="fa-solid fa-circle-exclamation"></i> ErrorPage
+          </Link>
+        </li>
+        <li>
+          {' '}
+          <Link to="/timer">
+            <i className="fa-sharp fa-solid fa-clock"></i> Timer
+          </Link>
+        </li>
+        <li>
+          {' '}
+          <Link to="/employeedetails">
+            <i className="fa-sharp fa-solid fa-address-card"></i>{' '}
+            EmployeeDetails
+          </Link>
+        </li>
+        <li>
+          {' '}
+          <Link to="/simpleclass">
+            <i className="fa-solid fa-landmark"></i> SimpleClass
+          </Link>
+        </li>
+        <li>
+          {' '}
+          <Link to="/dummy">
+            <i className="fa-solid fa-circle-xmark"></i> Dummy
+          </Link>
+        </li>
+        <li>
+          {' '}
+          <Link to="/employeelist"> EmployeeList</Link>
+        </li>
+        <li>
+          {' '}
+          <Link to="/change"> Change</Link>
+        </li>
+        <li>
+          {' '}
+          <Link to="/titlecounter"> TitleCounter</Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
 export default Navbar;
