44e1d135c02b75115e13f187958361d77d44907a
Rajan9176 <114674358+Rajan9176@users.noreply.github.com>
2022-10-19 15:06:46 +0530

    timer.jsx

--- src/pages/timer.jsx
+++ src/pages/timer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../css/timer.css';
 import { useState } from 'react';
 function Timer() 
 {
@@ -8,7 +9,7 @@ function Timer()
 
 
     return (
-        <div>
+        <div className="time">
             <div>
                 <h1>  <i class="fa-solid fa-business-time"></i> Current Time </h1>
                 <label>  {time.toLocaleTimeString()} </label>
