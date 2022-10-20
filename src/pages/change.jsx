1aebc82ea78f15b5cb123a4409f3bc615d7374e3
Rajan9176 <114674358+Rajan9176@users.noreply.github.com>
2022-10-20 20:10:48 +0530

    2 files modified

--- src/pages/change.jsx
+++ src/pages/change.jsx
@@ -1,28 +1,26 @@
 import React, { Component } from 'react';
 import '../css/change.css';
 class Change extends Component {
-  state=[25];
-  ChangeAge() {
-    // alert('hii');
-    // if (this.age === 25) {
-    //   this.setState({ age: (this.age = 45) });
-    // } else
-    //   () => {
-    //     this.setState({ age: (this.age = 25) });
-    //   };
+  state = { age: 25 };
+  changeAge() {
+    if (this.state.age === 25) {
+      this.setState({ age: 45 });
+    } else {
+      this.setState({ age: 25 });
+    }
   }
 
   render() {
     return (
       <div className="change">
-        <button onClick={() => this.state.age()}>Change</button>
+        <button onClick={() => this.changeAge()}>Change</button>
         <tr>
           <th className="name">Name</th>
           <th className="name">Age</th>
         </tr>
         <tr>
           <td className="age">Rajendran</td>
-          <td className="age">25</td>
+          <td className="age">{this.state.age}</td>
         </tr>
       </div>
     );
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,22 +1,41 @@
 import React from 'react';
 import '../css/login.css';
 function Login() {
+  function rajan() {
+    alert('2');
+    fetch('https://jsonplaceholder.typicode.com/comments', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/jason',
+      },
+      body: JSON.stringify({
+        userName: 'Rajendran',
+        Password: '9176160543',
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  }
+
   return (
-    
-      <div className="ram">
-        <h1 className="mr">Login Page</h1>
-        <label htmlfor="username">
-          <i class="fa-solid fa-user"></i>username
-        </label>
-        <input type="text"className="input"/>
-        <br />
-        <br />
-        <label htmlfor="password"><i class="fa-solid fa-lock"></i>password</label>
-        <input type="password" className="input"/>
-        <br />
-        <br /> <button className="raj">login</button>
-      </div>
-    
+    <div className="ram">
+      <h1 className="mr">Login Page</h1>
+      <label htmlfor="username">
+        <i class="fa-solid fa-user"></i>username
+      </label>
+      <input type="text" className="input" />
+      <br />
+      <br />
+      <label htmlfor="password">
+        <i class="fa-solid fa-lock"></i>password
+      </label>
+      <input type="password" className="input" />
+      <br />
+      <br />{' '}
+      <button onClick={() => rajan()} className="raj">
+        login
+      </button>
+    </div>
   );
 }
 export default Login;
