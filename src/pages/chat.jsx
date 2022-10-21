4e9724a1231826b6b56b5003687b131851d50712
Rajan9176 <114674358+Rajan9176@users.noreply.github.com>
2022-10-21 10:12:12 +0530

    chat.jsx

--- src/pages/chat.jsx
+++ src/pages/chat.jsx
@@ -5,8 +5,10 @@ function Chat() {
   const [message, setMessage] = useState('0');
   return (
     <div className="message">
-      <h1 className="hh">Chat Messages</h1>
-      <button onClick={() => setMessage('hi welcome')}>1</button>
+      <h1 className="hh">
+        <i class="fa-sharp fa-solid fa-comment"></i> Chat Messages
+      </h1>
+      <button  className="click" onClick={() => setMessage('hi welcome')}>1</button>
       <lable>{message}</lable>
       <button onClick={() => setMessage('i am good boy')}>2</button>
     </div>
