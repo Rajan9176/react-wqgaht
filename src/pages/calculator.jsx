c3b5b0a9c6765d1c8b1582217f2d1e8cf81ff53a
Rajan9176 <114674358+Rajan9176@users.noreply.github.com>
2022-10-21 10:26:11 +0530

    calculator.jsx

--- src/pages/calculator.jsx
+++ src/pages/calculator.jsx
@@ -5,31 +5,67 @@ function Calculator() {
     fetch('https://jsonplaceholder.typicode.com/comments')
       .then((res) => res.json())
       .then((Data) => console.log(Data));
+
+    function alert() {
+      alert();
+    }
   };
   return (
     <div className="rajan">
       <h1>Calculator</h1>
       <input type="text" />
       <br />
-      <button className="button">7</button>
-      <button className="button">8</button>
-      <button className="button">9</button>
-      <button className="button">c</button>
+      <button className="button" onClick={() => alert(7)}>
+        7
+      </button>
+      <button className="button" onClick={() => alert(8)}>
+        8
+      </button>
+      <button className="button" onClick={() => alert(9)}>
+        9
+      </button>
+      <button className="button" onClick={() => alert('c')}>
+        c
+      </button>
       <br />
-      <button className="button">4</button>
-      <button className="button">5</button>
-      <button className="button">6</button>
-      <button className="button">*</button>
+      <button className="button" onClick={() => alert(4)}>
+        4
+      </button>
+      <button className="button" onClick={() => alert(5)}>
+        5
+      </button>
+      <button className="button" onClick={() => alert(6)}>
+        6
+      </button>
+      <button className="button" onClick={() => alert('*')}>
+        *
+      </button>
       <br />
-      <button className="button">1</button>
-      <button className="button">2</button>
-      <button className="button">3</button>
-      <button className="button">-</button>
+      <button className="button" onClick={() => alert(1)}>
+        1
+      </button>
+      <button className="button" onClick={() => alert(2)}>
+        2
+      </button>
+      <button className="button" onClick={() => alert(3)}>
+        3
+      </button>
+      <button className="button" onClick={() => alert('-')}>
+        -
+      </button>
       <br />
-      <button className="button">0</button>
-      <button className="button">.</button>
-      <button className="button">=</button>
-      <button className="button">+</button>
+      <button className="button" onClick={() => alert(0)}>
+        0
+      </button>
+      <button className="button" onClick={() => alert('.')}>
+        .
+      </button>
+      <button className="button" onClick={() => alert('=')}>
+        =
+      </button>
+      <button className="button" onClick={() => alert('+')}>
+        +
+      </button>
     </div>
   );
 }
