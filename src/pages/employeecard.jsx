735816012d8567a41baa912f79f1bc2ecd0526f0
Rajan9176 <114674358+Rajan9176@users.noreply.github.com>
2022-10-31 11:18:08 +0530

    employeecard.jsx

--- src/pages/employeecard.jsx
+++ src/pages/employeecard.jsx
@@ -2,17 +2,34 @@ import React from 'react';
 import '../css/employee.css';
 
 function EmployeeCard() {
+  function mani() {
+    alert('mani');
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      headers: {
+        'Contant-Type': 'aplication/json',
+      },
+      body: JSON.stringify({
+        userName: 'mani',
+        Password: '987654',
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => consol.log(data));
+  }
   return (
     <div class="employee1">
-    <h1>Employee Id</h1>
-    <img src="bus.jpg"/><br/>
-    <span className="employee2">Rajan</span><br/><br/>
-    <span>200</span>
-    <div class="idcard">
-        <button>view</button>
+      <h1>Employee Id</h1>
+      <img src="bus.jpg" />
+      <br />
+      <span className="employee2">Rajan</span>
+      <br />
+      <br />
+      <span>200</span>
+      <div class="idcard">
+        <button onClick={() => mani()}>view</button>
+      </div>
     </div>
-
-  </div>
   );
 }
 export default EmployeeCard;
