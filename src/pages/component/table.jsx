86c444f948f71a41807ca89d128acec27d3594ff
Rajan9176 <114674358+Rajan9176@users.noreply.github.com>
2022-11-07 20:33:06 +0530

    table.jsx

--- src/pages/component/table.jsx
+++ src/pages/component/table.jsx
@@ -1,5 +1,35 @@
 import React from 'react';
 function Table(props) {
-  return <div> {props.data.length}</div>;
+  const genarateHead = () => {
+    var heading = Object.keys(props.data[0]);
+    return heading.map((each) => {
+      return <th>{each}</th>;
+    });
+  };
+
+  const genarateRow = () => {
+    return props.data.map((each) => {
+      return <tr> {genarateData(each)}</tr>;
+    });
+  };
+  const genarateData = (eachData) => {
+    var heading = Object.keys(props.data[0]);
+    return heading.map((each) => {
+      return <td>{eachData[each]}</td>;
+    });
+  };
+  return (
+    <div>
+      {' '}
+      {props.data.length}
+      <table>
+        <thead>
+          <tr>{genarateHead()}</tr>
+          {/* <td>{genarateHead()}</td> */}
+        </thead>
+        <tbody>{genarateRow()}</tbody>
+      </table>
+    </div>
+  );
 }
 export default Table;
